01b5ecd36cdd18f4e966a9a853bee4d0b3b00519
cruizba <carlos.ruizbal@gmail.com>
2021-12-31 03:42:22 +0100

    Remove unnecessary function

--- src/base-image/startup.sh
+++ src/base-image/startup.sh
@@ -1,22 +1,6 @@
 #!/bin/bash
 source /opt/bash-utils/logger.sh
 
-function wait_for_process () {
-    local max_time_wait=600
-    local process_name="$1"
-    local waited_sec=0
-    while ! pgrep "$process_name" >/dev/null && ((waited_sec < max_time_wait)); do
-        INFO "Process $process_name is not running yet. Retrying in 1 seconds"
-        INFO "Waited $waited_sec seconds of $max_time_wait seconds"
-        sleep 1
-        ((waited_sec=waited_sec+1))
-        if ((waited_sec >= max_time_wait)); then
-            return 1
-        fi
-    done
-    return 0
-}
-
 # Change password of userdocker
 echo "userdocker:${USER_PASSWORD}" | chpasswd
 
